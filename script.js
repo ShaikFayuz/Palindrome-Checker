6efcbe9f8d9691480c70d3e32a2983be62b92cf8
ShaikFayuz <fayuzahmed@gmail.com>
2024-09-30 17:33:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function isPalindrome(str) {
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    console.log(cleanStr);
+
+    const reversedStr = cleanStr.split('').reverse().join('');
+    console.log(reversedStr);
+
+    return cleanStr === reversedStr;
+}
+
+function PalindromeChecker() {
+    const inputText = document.getElementById("inputText");
+    const result = document.getElementById("result");
+
+    if (isPalindrome(inputText.value)) {
+        result.textContent = `"${inputText.value}" is a palindrome.`;
+    } else {
+        result.textContent = `"${inputText.value}" is not a palindrome.`;
+    }
+
+    result.classList.add('fadin');
+    inputText.value = "";
+}
+
+document.getElementById('checkButton').addEventListener("click", PalindromeChecker);
